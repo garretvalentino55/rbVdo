3a5dfb61c28cc6694ffe15acfe1a1a0853c7d14f
Garret Valentino <garret.valentino@gmail.com>
2020-02-09 14:30:07 -0600

    this is my update to my project i worked on it fo ra few hours and i want to make sure that it saves to prevent any mishaps

---
+++ day 5/Class2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_5
+{
+    class Dinosaur
+    {
+        //Member Variables (Has A)
+        public string dinosaurType;
+        public int healthLevel;
+        public int energyLevel;
+        public int attackPower;
+        public int damageDone;
+
+
+
+        //Constructor (spawner)
+
+        public Dinosaur()
+        {
+            this.dinosaurType = dinosaurType;
+            int health = healthLevel;
+            int stamina = energyLevel;
+            int attack = attackPower;
+            int strike = damageDone;
+
+
+
+
+        }
+
+
+
+        // Member Methods (Can Do)
+
+
+        public int Hit(Random random)
+        {
+            random.Next(1, 100);
+            int x = random.Next(1, 100);
+
+
+            if (x >= 70)
+            {
+                healthLevel -= 15;
+            }
+            return healthLevel;
+
+        }
+        public int Fight(Random random)
+        {
+            random.Next(1, 100);
+            int y = random.Next(1, 100);
+
+            if (y >= 60)
+            {
+                attackPower = 70;
+            }
+            else
+            {
+                attackPower = 0;
+            }
+            return attackPower;
+        }
+        public int Attack(Random random)
+        {
+            random.Next(1, 100);
+            int z = random.Next(1, 100);
+            if (z >= 65)
+            {
+                energyLevel -= 10;
+            }
+            else
+            {
+                energyLevel = 0;
+            }
+            return energyLevel;
+        }
+        public int Bite(Random random)
+        {
+            random.Next(1, 100);
+            int z = random.Next(1, 100);
+            if (z >= 65)
+            {
+                damageDone = 150;
+            }
+            else
+            {
+                damageDone = 0;
+            }
+            return damageDone;
+        }
+    }
+}
---
+++ day 5/weapons.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_5
+{
+    class weapons
+    {
+        //meber methods ( Has A)
+        public string name;
+        public int hitPower;
+
+
+
+        // constructor (spawner)
+        public weapons()
+        {
+            this.name = name;
+            int damage = hitPower;
+        }
+
+
+        // member varibales (Can Do)
+
+        public int Fight(Random random)
+        {
+            random.Next(1, 100);
+            int y = random.Next(1, 100);
+
+            if (y >= 60)
+            {
+                hitPower = 25;
+            }
+            else
+            {
+                hitPower = 0;
+            }
+            return hitPower;
+        }
+
+    }   
+}
